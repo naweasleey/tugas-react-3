f726d8734c93abfb89e018386346cb12a4b0b003
naweasleey <nailyna1212@gmail.com>
2023-06-03 16:23:39 +0700

    merapihkan baris

--- tugas-2/src/components/Login.jsx
+++ tugas-2/src/components/Login.jsx
@@ -3,11 +3,11 @@ import Tombol from "./Tombol";
 
 const Login = () => {
   return (
-     <>
-    <KotakTeks type="email" id="email" name="email"></KotakTeks>
-    <KotakTeks type="password" id="password" name="password"></KotakTeks>
-    <Tombol title="Log In"></Tombol>
-        </>
+    <>
+      <KotakTeks type="email" id="email" name="email"></KotakTeks>
+      <KotakTeks type="password" id="password" name="password"></KotakTeks>
+      <Tombol title="Log In"></Tombol>
+    </>
   )
 }
 
